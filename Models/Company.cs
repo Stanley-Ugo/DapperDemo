f0bbeec0e74fff28eafb19c066db283d1e4ff42d
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-13 23:23:56 +0100

    Adding Company Properties

--- Models/Company.cs
+++ Models/Company.cs
@@ -7,6 +7,11 @@ namespace DapperDemo.Models
 {
     public class Company
     {
-
+        public int CpmpanyId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
     }
 }
