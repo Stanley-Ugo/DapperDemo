fc35322e1d7292bfaf1e0a7894394a82703a0c35
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-14 00:11:41 +0100

    Adding a migration - addCompaniesToDb

--- Models/Company.cs
+++ Models/Company.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,8 @@ namespace DapperDemo.Models
 {
     public class Company
     {
-        public int CpmpanyId { get; set; }
+        [Key]
+        public int CompanyId { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
