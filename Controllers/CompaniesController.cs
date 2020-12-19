702c8f93a6087c0067ebb48b992eee472f8715e1
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-19 11:58:54 +0100

    Edit Companies Method Post

--- Controllers/CompaniesController.cs
+++ Controllers/CompaniesController.cs
@@ -80,7 +80,7 @@ namespace DapperDemo.Controllers
             return View(company);
         }
 
-        // POST: Companies/Edit/5
+        // POST: Companies/Edit/Id
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
