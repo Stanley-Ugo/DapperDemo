dffc8d91586057471e06b8afd5761684d7f36c41
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-12-19 11:57:55 +0100

    Create Companies Method

--- Controllers/CompaniesController.cs
+++ Controllers/CompaniesController.cs
@@ -43,7 +43,7 @@ namespace DapperDemo.Controllers
             return View(company);
         }
 
-        // GET: Companies/Create
+        // GET: Companies/Create 
         public IActionResult Create()
         {
             return View();
